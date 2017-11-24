1c229e3af783b008b35c0a5f8416a2c022de2896
John Hartman <john.f.d.hartman@gmail.com>
2017-11-24 15:55:44 +1000

    added routes for updating and posting puppies

--- queries.js
+++ queries.js
@@ -23,9 +23,9 @@ function getAllPuppies(req, res, next){
 		});	
 }
 
-function getSinglePuppy(req,res,next){
+function getSinglePuppy(req, res, next){
 	var pupID = parseInt(req.params.id);
-	db.one(`SELECT * FROM pups WHERE id = ${pupID}`)
+	db.one('SELECT * FROM pups WHERE id = $1', pupID)
 		.then(function (data) {
 			res.status(200)
 				.json({
@@ -40,10 +40,42 @@ function getSinglePuppy(req,res,next){
 
 }
 
+function createPuppy(req, res, next){
+	req.body.age = parseInt(req.body.age);
+	db.none('INSERT INTO pups(name, breed, age, sex)' +
+		'values(${name}, ${breed}, ${age}, ${sex})',
+		req.body)
+		.then(function () {
+			res.status(200)
+				.json({
+					status: 'success',
+					message: 'inserted one puppy'
+				});
+		})
+		.catch(function(err) {
+			return next(err);
+		});
+}
+
+function updatePuppy(req, res, next){
+	db.none('UPDATE pups SET name=$1, breed=$2, age=$3, sex=$4 WHERE id=$5',
+		[req.body.name, req.body.breed, parseInt(req.body.age), 
+			req.body.sex, parseInt(req.body.id)])
+		.then(function () {
+			res.status(200)
+				.json({
+					status: 'success',
+					message: 'updated one puppy'
+				});
+		}).catch(function(err) {
+			return next(err);
+		});
+}
+
 module.exports = {
 	getAllPuppies: getAllPuppies,
-	getSinglePuppy: getSinglePuppy
-	// createPuppy: createPuppy,
-	// updatePuppy: updatePuppy,
+	getSinglePuppy: getSinglePuppy,
+	createPuppy: createPuppy
+	updatePuppy: updatePuppy,
 	// removePuppy: removePuppy
 };
